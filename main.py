24b95b5d74c7c0876c4cbe59ed02473f9aed7147
Luca Spalierno <spalierno@gmail.com>
2020-11-23 20:21:27 +0100

    Add new endpoint.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/environment')
+def get_environment():
+    env_name = os.getenv('environment') or 'local'
+    return f'Env. name: {env_name}'
 
 
 if __name__ == '__main__':
