30d35b52826537d0f6b3b40fdd27b311b1a8f592
Luca Spalierno <spalierno@gmail.com>
2021-06-08 20:58:58 +0200

    Improve returned json format.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
 
@@ -8,13 +8,17 @@ app = Flask(__name__)
 
 env_name = os.getenv('environment') or 'local'
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 if env_name == 'production':
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.debug = True
 
 db = SQLAlchemy(app)
 
+# TODO: Replace with a proper `setup.py` script.
 db.create_all()
 
 
@@ -25,7 +29,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=False, nullable=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return 'User: %r' % self.username
 
 
 @app.route('/')
@@ -35,8 +39,8 @@ def hello_world():
 
 @app.route('/environment')
 def get_environment():
-    env_name = os.getenv('environment') or 'local'
-    return f'Env. name: {env_name}'
+    current_environment = os.getenv('environment') or 'local'
+    return f'Env. name: {current_environment}'
 
 
 @app.route('/user/new')
@@ -45,16 +49,16 @@ def create_user():
         guest = User(username='guest', email='guest@example.com')
         db.session.add(guest)
         db.session.commit()
+        new_user_id = guest.id
+        return make_response(jsonify(user_id=new_user_id))
     except Exception as e:
-        print(e)
-
-    return 'ok'
+        return make_response(jsonify(error=str(e)), 500)
 
 
 @app.route('/users')
 def get_users():
     users = User.query.all()
-    return jsonify(all_users=users)
+    return make_response(jsonify(all_users=[user.__repr__() for user in users]))
 
 
 if __name__ == '__main__':
